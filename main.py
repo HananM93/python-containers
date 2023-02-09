7f39fda2ef9ccb58d4c99b3070f0d6d0cf9b5e5d
Hanan mohamed <hananmohamed@Hanans-MacBook-Air.local>
2023-02-09 12:32:27 -0600

    exercise 1 done

---
+++ main.py
@@ -0,0 +1,8 @@
+# Exercise 1
+# Create a list named students containing some student names (strings).
+# Print out the second student's name.
+# Print out the last student's name.
+
+students = ['Ashley', 'Bri','Lacie','Andi']
+print(students[1])
+print(students[3])
