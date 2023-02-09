eacd94203eedae5b8595aa617e289b1938779381
Hanan mohamed <hananmohamed@Hanans-MacBook-Air.local>
2023-02-09 13:05:57 -0600

    exercise 6 done

--- main.py
+++ main.py
@@ -47,3 +47,22 @@ print(f"I grew up in {home_town['city']}, {home_town['state']} - population: {ho
 
 for key,val in home_town.items():
     print(f"{key} = {val}")
+
+
+# Exercise 6
+# Create an empty list named cohort.
+# Using a for loop, add one dictionary to the cohort list for each student name. Each dictionary should have this shape: 
+
+# {
+# 	'student': 'Tina',
+# 	'fav_food': 'Cheeseburger'
+# }
+#Iterate over cohortprinting out each element.
+
+cohort = []
+for idx, students in enumerate(students):
+    cohort.append({
+        'student': students,
+        'fav_food': foods[idx]
+    })
+    print(cohort[idx])
